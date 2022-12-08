a4d0e70174926d1e8319c6c5aa52a7c79f152610
Hossam Mahmoud <76559078+HossamMahmoudkhedr@users.noreply.github.com>
2022-12-08 16:41:50 +0200

    Update Help-VAsilisa-the-Wise-21.cpp

--- Help-VAsilisa-the-Wise-21.cpp
+++ Help-VAsilisa-the-Wise-21.cpp
@@ -22,3 +22,92 @@ int main()
 	}
 	cout<<-1<<endl;
 }
+
+
+
+
+
+
+
+
+/* ************************** Bullsh*t ************************** */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    // X = (r1 - d2 + c1)/ 2
+    // Y = (d2 - r2 - d1 + r1) /2
+    // Z = d2 - Y
+    // O = d1 - X
+    int x, y, z ,o, r1, r2, c1, c2, d1 ,d2, remander;
+    cin >> r1 >> r2 >> c1 >> c2 >> d1 >> d2;
+
+    x= abs((r1 - d2 + c1)/ 2);
+    y = abs((d2 - r2 - d1 + r1) /2);
+    z = abs(d2 - y);
+    o = abs(d1 - x);
+    if(x == 0 || y == 0 || z == 0 || o ==0){
+        cout << -1 << endl;
+    }else{
+        while(r1 != x + y && r2 != z + o && c1 != x + z && c2 != y + o && d1 != x + o && d2 != y + z){
+        if(r1 != x + y){
+                remander = r1 - (x + y);
+            if(x > y){
+                x += remander;
+            }else{
+                y += remander;
+            }
+        }
+         if(r2 != z + o){
+            remander = r2 - (z + o);
+                if( z > o){
+                    z +=remander;
+                }else{
+                    o +=remander;
+                }
+        }
+         if( c1 != x + z){
+            remander = c1 - (x + z);
+            if(z > x){
+                z += remander ;
+            }else{
+                x += remander;
+            }
+        }
+        if(c2 != y + o){
+                remander = c2 - (y + o);
+            if(y > o){
+                y += remander;
+            }else{
+                o += remander;
+            }
+        }
+        if(d1 != x + o){
+            remander = d1 - (x + o);
+            if(x > o){
+                x += remander;
+            }else{
+                o += remander;
+            }
+        }
+         if(d2 != y + z){
+            remander = d2 - (y + z);
+            if(y > z){
+                y += remander;
+            }else{
+                z += remander;
+            }
+
+        }
+        }
+
+    cout << abs(x) << " " << abs(y) << endl;
+    cout << abs(z) << " " << abs(o) << endl;
+    }
+
+
+    return 0;
+}
